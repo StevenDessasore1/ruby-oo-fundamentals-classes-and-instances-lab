e9965a085b98a21f459b661b38f65bfe4c75130f
StevenDessasore <stevendessasoreba@gmail.com>
2020-11-21 15:37:29 -0500

    commit tree b3c952cb8c49eb4d1645d7f08ba772b0335a4dec

---
+++ lib/dog.rb
@@ -0,0 +1,16 @@
+require_relative '../lib/dog'
+
+
+class Dog
+    def bark
+        puts "Woof!"
+    end
+    
+    def sit
+        puts "The Dog is sitting"
+    end
+end
+
+fido = Dog.new
+snoopy = Dog.new
+lassie = Dog.new
